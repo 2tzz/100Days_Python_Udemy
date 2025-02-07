e7a1dda35d192f285a2ebc2b53a801d77a85a44d
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-07 17:00:42 +0530

    initial commit

---
+++ day1.py
@@ -0,0 +1,22 @@
+# print("hellow world")
+
+# print("hellow world \n hellow world\nhellow world")
+
+# print("hellow" + "    Angelo")
+
+# x = input("enter your name")
+
+# print("hellow   "+  x)
+
+
+# print(len(x) )
+
+# print(len(input("what is your name :")))
+user_name=input("What is your name : ")
+pet_name = input("What is your pets name :")
+
+print("Your brand name could be "+ user_name + " " +pet_name)
+
+
+
+
---
+++ day2(Tip calculator).py
@@ -0,0 +1,35 @@
+# mystry = 734_529.678
+
+
+# print(type(mystry))
+
+
+
+
+
+
+# print("number of letters in your name :" , len(input("enter your name here")) )
+
+
+
+# bmi = 83 / 1.65  ** 2 
+# print(round(bmi,2))
+
+
+
+# score = 0
+# height = 1.8
+
+# is_winning = True
+
+# print(f"your score is = {score}  yor height is = {height}  are you winning = {is_winning}")
+x = int(input("Enter your total  bill : "))
+
+print(f"your total bill is : {x} $")
+y = int(input("enter how  much persentage of tip u give (10%,15%,20%):"))
+
+total = x +  x / 100 * y
+
+
+print(f"yor total will be : {total} $")
+
