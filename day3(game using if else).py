9f33008857cd499e61db3897712a52522212b221
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-08 18:31:22 +0530

    updated roller coster

--- day3(game using if else).py
+++ day3(game using if else).py
@@ -1,25 +1,36 @@
-# print("welcome to roller coster !")
-# height = int(input("Enter your height : "))
+print("welcome to roller coster !")
+height = int(input("Enter your height : "))
 
 
-# if height >=  120 :
-#     print(" you can ride the rollercoster")
+if height >=  120 :
+    print(" you can ride the rollercoster")
 
-#     age =  int(input("Enter your age : "))
+    age =  int(input("Enter your age : "))
 
-#     if age <= 12:
-#         print("please pay 5 $")
 
-#     elif age <= 18:
-#         print("please pay 7 $")
+    bill = 0
 
-#     else :
-#         print("please pay 12 $")
+    if age <= 12:
+        print("please pay 5 $")
+        bill = 5
+    elif age <= 18:
+        print("please pay 7 $")
+        bill = 7
+    else :
+        print("please pay 12 $")
+        bill = 12
 
+    pic = input("do you want a photo?")
+
+    if pic == 'yes' and age <= 12:
+            bill = bill + 3
+            print(f"yor total will be  : {bill} $")
     
+    else:
+          print(f"please pay the total : {bill} $ ")
 
-# else :
-#     print("sorry you can't ride this get taller to ride this")
+else :
+    print("sorry you can't ride this get taller to ride this")
 
 
 
