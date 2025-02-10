21e30f35db11e12ae118c9ee92cd6762fe072d8c
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-10 20:06:07 +0530

    random lesson done

---
+++ day4.py
@@ -0,0 +1,23 @@
+# import random
+
+# random_int = random.randint(1 , 10)
+
+# print(random_int)
+
+# random_float = random.random() * 10
+
+# print (random_float)
+
+# random_float = random.uniform(2 , 10 )
+# print(random_float)
+
+
+# random_int = random.randint(0,1)
+
+
+# if random_int == 1:
+#     print("Heads")
+
+# else :
+#     print("tails")
+
