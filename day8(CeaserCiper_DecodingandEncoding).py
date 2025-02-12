2c0425d5a726d87d8085042f4111f6b3820f2850
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-12 19:21:01 +0530

    done until caeser cipher

---
+++ day8(CeaserCiper_DecodingandEncoding).py
@@ -0,0 +1,38 @@
+# def greet():
+#     print("hellow")
+#     print("how are you")
+#     print("bye")
+
+
+# greet()
+
+# # def greet_with_name(name) :
+
+# #     print(f"hellow {name} !")
+# #     print(f"how are you {name} ?")
+# #     print(f"bye  {name} !")
+
+# # greet_with_name('thiyura')
+
+# # def life_in_weeks(age):
+    
+# #     return (90 - age ) * 12 * 4
+    
+    
+# # yourage = int(input("enter your age : "))
+
+# # x=life_in_weeks(yourage)
+
+# # print(f"you have {x} weeks left.")
+
+# def greet_with_location(name,location):
+
+#     print(f"hellow {name} !")
+#     print(f"greetings from {location} ! ")
+
+
+# greet_with_location('thiyura', 'ingiriya')
+
+
+
+
