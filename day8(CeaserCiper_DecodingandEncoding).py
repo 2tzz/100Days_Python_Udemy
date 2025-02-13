8543cdee02bf9ec977e4d6a3e534f2a67e2bea43
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-13 13:21:36 +0530

    found the logic ez

--- day8(CeaserCiper_DecodingandEncoding).py
+++ day8(CeaserCiper_DecodingandEncoding).py
@@ -33,6 +33,35 @@
 
 # greet_with_location('thiyura', 'ingiriya')
 
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+direction = input("type 'encode' to encrypt , type 'decode' to decrypt :\n").lower()
+text = input("type your messege : \n").lower()
+shift = int(input("Type the shift number :"))
 
 
+def encode_process(text , shift):
+    text_list = []
+    output_list = []
+    char_position = 0
 
+    for  i in text :
+        text_list.append(i)
+    
+    for j in text_list :
+        for k in alphabet :
+            if j == k:
+                char_position = alphabet.index(k)
+                if char_position + shift <= 26:
+                    char_position = char_position + shift
+                    output_list.append(alphabet[char_position])
+                elif char_position + shift > 26:
+                    char_position = char_position + shift - 26
+                    output_list.append(alphabet[char_position])
+
+            
+
+    print(output_list)
+
+
+encode_process(text , shift)
