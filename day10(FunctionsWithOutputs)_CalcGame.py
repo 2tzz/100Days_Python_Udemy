00f58ce3fce06452d667102a2af056d70ae6616d
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-17 12:55:12 +0530

    Advance claculator done

--- day10(FunctionsWithOutputs)_CalcGame.py
+++ day10(FunctionsWithOutputs)_CalcGame.py
@@ -31,3 +31,56 @@
         
 # print(bool(is_leap_year(input_y)))   
 
+
+
+def calculator_fun(input_val1 , input_val2 , operator) :
+
+    if operator == '+' :
+        return input_val1 + input_val2
+    
+    elif operator == '-' :
+
+        return input_val1 - input_val2
+    
+    elif operator == '*' :
+        return input_val1 * input_val2
+    
+    elif operator == '/' :
+        return input_val1 / input_val2
+    
+    else:
+        print("operator error")
+
+
+direction = 'y'
+input_1 = 0
+input_2 = 0
+op_select = ''
+output = 0
+
+input_1 = float(input("What is your frist number : "))
+
+while direction == 'y' or 'n' :
+
+    
+
+    op_select = input(" + \n - \n * \n / \n pick an operation : ")
+
+    input_2 = float(input("Whats the next number : "))
+
+    output = calculator_fun(input_1 , input_2, op_select)
+
+    print (f"{input_1} {op_select} {input_2} = {output}")
+
+    direction = input(f"Type 'y' to continue calculating with {output} , or type 'n to start a new calculation : ")
+
+    if direction == 'y' :
+
+        input_1 == 0
+        input_1 = output
+        
+        print(f"frist number {input_1}")
+
+    elif direction == 'n' :
+
+        input_1 = float(input("What is your frist number : "))
