0d2ca55f58498d562b18b0200b526ceb80274338
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-18 07:24:48 +0530

    begining day 12

--- day11(BLACKJACKgame).py
+++ day11(BLACKJACKgame).py
@@ -1,3 +1,6 @@
+#my implementation
+
+
 
 import random
 
@@ -10,59 +13,69 @@ dealer_total = 0
 temp = 0
 condition = ''
 
-player_cards.append(random.choice(cards))
-player_cards.append(random.choice(cards))
-dealer_cards.append(random.choice(cards))
+cont = input("Do you want to play BLACKJACK ? 'y' or 'n' ? ")
 
-print(f"your cards  : {player_cards}")
-print(f"Dealer cards  : {dealer_cards}")
-
-condition = input("Do you want to add another card ? type 'y' for yes 'n' for no   : ")
-
-if condition == 'y':
+while cont == 'y' :
 
     player_cards.append(random.choice(cards))
-    
-    plyer_total = sum(player_cards)
-
-    print(f" Your cards - {player_cards} ")
-    
-
+    player_cards.append(random.choice(cards))
     dealer_cards.append(random.choice(cards))
-    print(f" dealer cards - {dealer_cards} ")
-      
-    if plyer_total > 21 :
-        print("You loose")
+
+    print(f"your cards  : {player_cards}")
+    print(f"Dealer cards  : {dealer_cards}")
+
+    condition = input("Do you want to add another card ? type 'y' for yes 'n' for no   : ")
+
+    if condition == 'y':
+
+        player_cards.append(random.choice(cards))
         
-    
-    elif dealer_total < random.randint(13, 15):
-        print("** Dealer pulls another card **")
+        plyer_total = sum(player_cards)
+
+        print(f" Your cards - {player_cards} ")
+        
+
         dealer_cards.append(random.choice(cards))
         print(f" dealer cards - {dealer_cards} ")
-        plyer_total = sum(player_cards)
-        dealer_total = sum(dealer_cards)
+        
+        if plyer_total > 21 :
+            print("\nYou loose\n\n")
+            
+        
+        elif dealer_total < random.randint(13, 15):
+            print("** Dealer pulls another card **")
+            dealer_cards.append(random.choice(cards))
+            print(f" dealer cards - {dealer_cards} ")
+            plyer_total = sum(player_cards)
+            dealer_total = sum(dealer_cards)
 
-        if dealer_total > 21 or dealer_total < plyer_total :
-            print("\n\n\nCongrads !! you Win !!")    
+            if dealer_total > 21 or dealer_total < plyer_total :
+                print("\nCongrads !! you Win !!\n\n")    
 
-elif condition == 'n':
-    
-    print(f" Your cards - {player_cards} ")
-    
+    elif condition == 'n':
+        
+        print(f" Your cards - {player_cards} ")
+        
 
-    dealer_cards.append(random.choice(cards))
-    print(f" dealer cards - {dealer_cards} ")
-      
-    if plyer_total > 21 :
-        print("\n\n\nYou loose")
-
-    elif dealer_total < random.randint(13, 15):
-        print("** Dealer pulls another card **")
         dealer_cards.append(random.choice(cards))
         print(f" dealer cards - {dealer_cards} ")
-        plyer_total = sum(player_cards)
-        dealer_total = sum(dealer_cards)
+        
+        if plyer_total > 21 :
+            print("\nYou loose\n\n")
+
+        elif dealer_total < random.randint(10, 13):
+            print("** Dealer pulls another card **")
+            dealer_cards.append(random.choice(cards))
+            print(f" dealer cards - {dealer_cards} ")
+            plyer_total = sum(player_cards)
+            dealer_total = sum(dealer_cards)
 
 
-        if dealer_total > 21 or dealer_total < plyer_total :
-            print("\n\n\nCongrads !! you Win !!")
+            if dealer_total > 21 or dealer_total < plyer_total :
+                print("\nCongrads !! you Win\n\n !!")
+
+    player_cards.clear()    
+    dealer_cards.clear()
+    plyer_total = 0
+    dealer_total = 0
+    temp = 0   
