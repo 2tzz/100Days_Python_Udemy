3b6e5ba48d5f1ab9738c865a9d43d8522f73323b
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-18 20:31:03 +0530

    halfway day 12

--- day12(number_guess_game).py
+++ day12(number_guess_game).py
@@ -1,3 +1,5 @@
+import random
+
 # def is_prime(num):
 #     temp1 = 0
 #     temp2 = 0
@@ -20,4 +22,36 @@
     
          
 # print (is_prime(2))
+
+print("Welcome to number guessing game ")
+
+numbers = []
+
+for i in range (0 , 100) :
+    numbers.append(i+1)
+
+print(numbers)
+guess = random.choice(numbers)
+
+
+    
+user_input = 0
+
+while guess != user_input :
+
+    user_input = int(input("Make a guess : "))
+
+    if user_input == guess :
+        print(f"you guess the number !! number was {guess}")
+
+    elif user_input < guess :
+        print("too low")
+
+    elif user_input < guess :
+        print("too high")
+
+
+
+
+
 
