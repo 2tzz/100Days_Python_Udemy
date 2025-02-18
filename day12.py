96c2cefd02339a5aa3c2626b8ac49b5a56994672
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-18 17:40:08 +0530

    did some changes

--- day12.py
+++ day12.py
@@ -1,13 +1,22 @@
 def is_prime(num):
-    
-    for i in range(2,num-1) :
-        temp = 0
-        if num % 1 == 0 and num % num ==0 and num % i == 0 :
-            temp += 1
-    if temp != 0 :
+    temp1 = 0
+    temp2 = 0
+    for i in range(2,(num-1)) :
+
+        if num % 1 == 0 and num % num ==0:
+            temp1 += 1
+
+        if num % i == 0 :
+            temp2 += 1
+
+
+    if temp1 > 0 and temp2 == 0 :
+        return True
+    elif num == 2 :
+        return True
+
+    elif temp2 > 0 :
         return False
     
-    else :
-        return True
-        
-print (is_prime(4))
+         
+print (is_prime(2))
