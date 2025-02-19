d46295ba2f6e43d05e9a698a30b6f62d4d5407db
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-19 20:44:55 +0530

    mm

--- day13.py
+++ day13.py
@@ -1,8 +1,17 @@
 #learning debugging
 
-from random import randint
+# from random import randint
 
-dice_images = ["0" , "0" , "0" , "0", "0" ,"0"]
+# dice_images = ["0" , "0" , "0" , "0", "0" ,"0"]
 
-dice_num = 6
-print(dice_images[dice_num])
+# dice_num = 6
+# print(dice_images[dice_num])
+
+
+year = int(input("What is your year of birth : "))
+
+if 1980 < year <= 1994 :
+    print("You are Millenial")
+
+elif  1994 < year :
+    print("you are Gen Z")
