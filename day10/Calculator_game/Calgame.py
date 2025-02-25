da266b5390d1898e9ed172929f685c628fc1d0e9
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-25 19:12:44 +0530

    commits

--- day10/Calculator_game/Calgame.py
+++ day10/Calculator_game/Calgame.py
@@ -1,35 +1,4 @@
-
-
-# f_name  = input("Enter your Frist name  : ")
-# l_name  = input("Enter your last Name : ")
-
-# def format_name (frist_name , Last_name) :
-
-#     frist_name = frist_name.capitalize()    #you can also use .title()
-#     Last_name = Last_name.capitalize()
-
-#     print (f"Yor full name is {frist_name} {Last_name}")
-
-
-# format_name(f_name , l_name)
-
-
-# def is_leap_year(year):
-#     if year % 4 != 0:
-#         return 0
-    
-#     elif year % 4 ==0 :
-        
-#         if year % 100 ==0 and year % 400 != 0  :
-            
-#             return 0
-        
-#         else :
-#             return 1
-  
-# input_y = int(input()  )    
-        
-# print(bool(is_leap_year(input_y)))   
+from art import logo
 
 
 
@@ -51,6 +20,9 @@ def calculator_fun(input_val1 , input_val2 , operator) :
     else:
         print("operator error")
 
+
+
+print(logo)
 
 direction = 'y'
 input_1 = 0
