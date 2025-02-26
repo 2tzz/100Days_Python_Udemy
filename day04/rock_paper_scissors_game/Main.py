5d7238243d32c0b4c5fe214e4f7332be3125188d
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-26 17:19:32 +0530

    g

--- day04/rock_paper_scissors_game/Main.py
+++ day04/rock_paper_scissors_game/Main.py
@@ -1,33 +1,5 @@
 import random
 
-# random_int = random.randint(1 , 10)
-
-# print(random_int)
-
-# random_float = random.random() * 10
-
-# print (random_float)
-
-# random_float = random.uniform(2 , 10 )
-# print(random_float)
-
-
-# random_int = random.randint(0,1)
-
-
-# if random_int == 1:
-#     print("Heads")
-
-# else :
-#     print("tails")
-
-# friends = ["alice","bob","Charlie","David","Emanuel"]
-
-# num = random.randint (0 , 4)
-
-# print(friends[num])
-
-
 print("hellow welcome to rock paper scissors game. ")
 
 user_input = int(input("What do you want to choose ? type 0 for Rock, 1 for Paper or 2 for Scissors"))
