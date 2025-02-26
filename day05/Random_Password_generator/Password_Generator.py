eb99aebfa47f4994c0bce7c0887e08cf5fa198aa
2tzz <157218559+2tzz@users.noreply.github.com>
2025-02-26 16:52:51 +0530

    new file

--- day05/Random_Password_generator/Password_Generator.py
+++ day05/Random_Password_generator/Password_Generator.py
@@ -1,51 +1,5 @@
 
 import random
-# fruits = ['apple','peach','pear']
-
-# for fruit in fruits:
-#     print (fruit)
-
-# student_scores = [150 , 142 , 185 , 120 , 171 , 184 , 149 , 24 , 59 , 68 , 199 , 78 , 65 , 89 , 86 ]
-
-# max_score = 0
-
-# for i in student_scores :
-
-
-#     if i > max_score :
-#         max_score = i
-
-
-# print ("max score : " , max_score)
-
-# for number in range(1 , 10 , 2):
-#     print(number)
-
-#challenge 1
-
-# sum = 0
-
-# for num in range (1,101):
-    
-#     sum = sum + num
-
-# print(sum)
-
-#challenge 2 ez
-
-# for num in range(1,101):
-    
-#     if num % 3 == 0 and num % 5 == 0:
-#         print ("FizzBuzz")
-    
-#     elif num % 3 == 0 :
-#         print("Fizz") 
-        
-#     elif num % 5 == 0 :
-#         print("Buzz")
-        
-#     else:
-#         print (num)
 
 
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','Z','X']
