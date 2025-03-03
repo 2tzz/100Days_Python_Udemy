cfbdde9fb3cb99ac7db0f81b1484e003adfeb2a4
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-03 18:07:16 +0530

    project with oop concepts ongoing

--- Logs/day16/Coffee_Mchine(Using OOP)/main.py
+++ Logs/day16/Coffee_Mchine(Using OOP)/main.py
@@ -1,3 +1,23 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+menux = Menu()
+
+menux.__init__()
+
+order = input(f"What kind of cofee do you need ? type ({menux.get_items()}) : ")
+
+print(order)
+
+if order  == 'latte' or order  == 'cappuccino' or order  == 'espresso' :
+    water_amount = menux.find_drink(order).ingredients["water"]
+    coffee_amount = menux.find_drink(order).ingredients["coffee"]
+    milk_amount = menux.find_drink(order).ingredients["milk"]
+    order_cost = menux.find_drink(order).cost
+    print(water_amount , coffee_amount , milk_amount)
+
+else :
+    print(menux.find_drink(order))
+
