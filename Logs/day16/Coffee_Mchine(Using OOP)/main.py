08e8c39caa2c57c0520d6eb8e0a686eab008200a
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-04 11:35:10 +0530

    found this shit

--- Logs/day16/Coffee_Mchine(Using OOP)/main.py
+++ Logs/day16/Coffee_Mchine(Using OOP)/main.py
@@ -4,19 +4,31 @@ from money_machine import MoneyMachine
 
 
 menux = Menu()
+coffeemaker = CoffeeMaker ()
+moneymachine = MoneyMachine ()
 
 menux.__init__()
+coffeemaker.__init__()
+moneymachine.__init__()
 
-order = input(f"What kind of cofee do you need ? type ({menux.get_items()}) : ")
 
-print(order)
+
+
+order = input(f"What kind of cofee do you need ? type ({menux.get_items()}) or type (report) to get a report : ")
+
 
 if order  == 'latte' or order  == 'cappuccino' or order  == 'espresso' :
-    water_amount = menux.find_drink(order).ingredients["water"]
-    coffee_amount = menux.find_drink(order).ingredients["coffee"]
-    milk_amount = menux.find_drink(order).ingredients["milk"]
-    order_cost = menux.find_drink(order).cost
-    print(water_amount , coffee_amount , milk_amount)
+
+
+    if moneymachine.make_payment(menux.find_drink(order).cost) == True :
+
+        coffeemaker.make_coffee(menux.find_drink(order))
+    
+
+elif order == 'report':
+
+    coffeemaker.report()
+    moneymachine.report()
 
 else :
     print(menux.find_drink(order))
