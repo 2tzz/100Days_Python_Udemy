93c5e1bbbfd8caae23fee559ee43566fd12d2aa1
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-05 10:48:04 +0530

    ready to create the quiz game using oop

--- Logs/day17/main.py
+++ Logs/day17/main.py
@@ -1,4 +1,24 @@
 class User :
+    def __init__(self,user_id , username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self):
+        self.followers += 1 
+        self.following += 1
+
+    def report (self):
+
+        print (self.id , self.username , self.followers , self.following)
 
 
-user 1 = User ()
+user_1  = User('001' , 'pamkaya')
+
+
+user_1.report()
+
+user_1.follow()
+
+user_1.report()
