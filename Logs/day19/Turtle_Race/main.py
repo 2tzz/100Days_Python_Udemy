19628c2493bfee7f7dd157057d42149111a37442
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-08 18:30:00 +0530

    changed bg color and turtle tails

--- Logs/day19/Turtle_Race/main.py
+++ Logs/day19/Turtle_Race/main.py
@@ -8,6 +8,7 @@ is_race_on = False
 
 screen = Screen()
 screen.setup(width = 500 ,height =  400)
+screen.bgcolor("grey")
 
 
 colors = ["red","orange","yellow","green","blue"]
@@ -15,21 +16,6 @@ colors = ["red","orange","yellow","green","blue"]
 y = -100
 all_turtles = []
 
-
-# new_turtle = Turtle(shape="turtle" )
-# new_turtle.color("Red")
-
-# blue = Turtle(shape="turtle" )
-# blue.color("Blue")
-
-# green = Turtle(shape="turtle" )
-# green.color("green")
-
-# yellow = Turtle(shape="turtle" )
-# yellow.color("Yellow")
-
-# purple = Turtle(shape="turtle" )
-# purple.color("Purple")
 
 
 def draw_finishline():
@@ -40,54 +26,12 @@ def draw_finishline():
     refree.left(90)
     refree.forward(240)
 
-# def teleport_to_start():
-#     new_turtle.teleport(-230, -100)
-#     blue.teleport(-230, -50)
-#     green.teleport(-230, 0)
-#     yellow.teleport(-230, 50)
-#     purple.teleport(-230, 100)
-
-# def turtle_run ():
-
-#     a = 0
-#     b = 0
-#     c = 0
-#     d = 0
-#     e = 0
-
-#     distance = 40
-
-#     speed = 1
-
-#     new_turtle.speed(speed)
-#     blue.speed(speed)
-#     green.speed(speed)
-#     yellow.speed(speed)
-#     purple.speed(speed)
-
-#     while a <= distance or b <= distance or c <= distance or d <= distance or e <= distance :
-
-#         a += random.randint(1 ,5)
-#         b += random.randint(1 ,5)
-#         c += random.randint(1 ,5)
-#         d += random.randint(1 ,5)
-#         e += random.randint(1 ,5)
-
-        
-
-#         new_turtle.forward(a)
-#         blue.forward(b)
-#         green.forward(c)
-#         yellow.forward(d)
-#         purple.forward(e)
-
-        
-
 
 for i in range(0 , 5) :
     
     new_turtle = Turtle(shape="turtle")
     new_turtle.color(colors[i])
+    new_turtle.penup()
     new_turtle.teleport(-230, y)
     y += 50
 
