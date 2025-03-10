6ca4a316f75dd0aafc764179c3da70401234f491
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-10 13:07:03 +0530

    manged to make snake body with moving forward using squares

--- Logs/day20/Snake_Game/main.py
+++ Logs/day20/Snake_Game/main.py
@@ -1,28 +1,45 @@
 from turtle import Turtle , Screen
+import time
 
 
 screen = Screen ()
 screen.setup(width=600 ,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 x = 0
 y = 0
 
 
-for turtle in range (0,3) :
+starting_positions = [(0,0) , (-20,0) ,(-40,0)]
+segments  = []
+
+for positions in  starting_positions :
     
 
     new_turtle = Turtle(shape="square")
     new_turtle.color("White")
     new_turtle.penup()
-    new_turtle.shapesize(1 , 1)
-    new_turtle.teleport (x , y)
-
-    x += 20
-
+    new_turtle.goto(positions)
+    segments.append(new_turtle)
+    
 
 
+game_is_on = True
+
+while game_is_on :
+    screen.update()
+    time.sleep(0.1)
+    
+    for  seg_num in range (len(segments) - 1, 0 , -1) :
+        new_x = segments[seg_num-1].xcor()
+        new_y = segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x,new_y)
+
+    segments[0].forward(20)
+    
+        
 
 
 
