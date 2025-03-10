bcb1236a8b9ad3ffe55d5cab08e0496dbfb7b437
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-10 15:19:42 +0530

    crated using oop

--- Logs/day20/Snake_Game/main.py
+++ Logs/day20/Snake_Game/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle , Screen
+from turtle import  Screen
+from snake import Snake
 import time
 
 
@@ -8,22 +9,7 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-x = 0
-y = 0
-
-
-starting_positions = [(0,0) , (-20,0) ,(-40,0)]
-segments  = []
-
-for positions in  starting_positions :
-    
-
-    new_turtle = Turtle(shape="square")
-    new_turtle.color("White")
-    new_turtle.penup()
-    new_turtle.goto(positions)
-    segments.append(new_turtle)
-    
+snake = Snake()
 
 
 game_is_on = True
@@ -32,13 +18,7 @@ while game_is_on :
     screen.update()
     time.sleep(0.1)
     
-    for  seg_num in range (len(segments) - 1, 0 , -1) :
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x,new_y)
-
-    segments[0].forward(20)
-    
+    snake.move()
         
 
 
