7ad976eacbc263e6f21e367e656b9a6b17c0365b
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-13 13:42:03 +0530

    car created on the screen

--- Logs/day23/car_manager.py
+++ Logs/day23/car_manager.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    
+
+    def __init__(self):
+        super().__init__()
+
+        self.shape("square")
+        self.shapesize(1 , 2)
+        self.penup()
+        self.setheading(180)
+        
+    def stating_pos(self):
+
+        self.goto(280 , 0)
--- Logs/day23/main.py
+++ Logs/day23/main.py
@@ -8,7 +8,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+car_manager = CarManager()
+
+car_manager.stating_pos()
 game_is_on = True
+
 while game_is_on:
+    
+    car_manager.forward(3)
     time.sleep(0.1)
     screen.update()
+    
