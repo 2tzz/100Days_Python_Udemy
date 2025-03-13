797f3292d0ea2ce700643266d0b16a3e5d20d446
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-13 19:22:28 +0530

    created turtle and added move ments when user press up

--- Logs/day23/car_manager.py
+++ Logs/day23/car_manager.py
@@ -1,21 +1,41 @@
 from turtle import Turtle
+import random
+import time
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
     
 
     def __init__(self):
-        super().__init__()
-
-        self.shape("square")
-        self.shapesize(1 , 2)
-        self.penup()
-        self.setheading(180)
         
-    def stating_pos(self):
 
-        self.goto(280 , 0)
+        self.cars = []
+        
+
+    def create_cars (self):
+        for car in range(0,10) :
+            self.add_car((random.randint(280 , 500) , random.randint(-280,280)))
+
+  
+
+    def add_car (self , position):
+        new_car = Turtle ()
+        new_car.shape("square")
+        new_car.shapesize(1 , 2)
+        new_car.penup()
+        new_car.setheading(180)
+        new_car.color(COLORS[random.randint(0,5)])
+        new_car.goto(position)
+        self.cars.append(new_car)
+    
+    def car_fd(self):
+
+        for i in range(0,10) :
+            if self.cars[i].xcor() < -280 :
+                self.cars[i].goto(random.randint(280 , 500) , random.randint(-280,280) )
+            self.cars[i].forward((MOVE_INCREMENT))
+            
--- Logs/day23/main.py
+++ Logs/day23/main.py
@@ -3,19 +3,29 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-car_manager = CarManager()
+carmanager = CarManager()
+player = Player()
 
-car_manager.stating_pos()
+carmanager.create_cars()
+
+screen.listen()
+screen.onkey(player.go , "Up" )
+
 game_is_on = True
 
 while game_is_on:
-    
-    car_manager.forward(3)
+
     time.sleep(0.1)
+    for i in range(0,10) :
+            if carmanager.cars[i].xcor() < -280 :
+                carmanager.cars[i].goto(random.randint(280 , 500) , random.randint(-280,280) )
+            carmanager.cars[i].forward(10)
     screen.update()
+    
     
