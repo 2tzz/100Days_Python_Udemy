9f58ab68061f2f39aac50b2c86d4227152609675
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-17 21:00:49 +0530

    changed falders and manged to save high score permenrntly

--- Logs/day24/exersises/main.py
+++ Logs/day24/exersises/main.py
@@ -1,4 +1,4 @@
-file = open("my_file.txt")
-content = file.read()
-print(content)
-file.close()
+
+with open("my_file.txt" , mode = "a") as file :
+    file.write("\n new text2")
+
---
+++ Logs/day24/snake_game/test.py
@@ -0,0 +1,8 @@
+with open("data.txt" , mode = "r") as file :
+         high_score =  file.read() 
+
+# with open("data.txt" , mode = "w") as file :
+#     file.write("pakaya")
+
+
+print(high_score)
