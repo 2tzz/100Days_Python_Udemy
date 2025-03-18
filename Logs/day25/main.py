583125511a6bc642850f870f6c8c4a98ce9162c0
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-18 11:23:43 +0530

    working with pandas

---
+++ Logs/day25/main.py
@@ -0,0 +1,31 @@
+# new_data = []
+
+# with open(r"weather_data.csv") as data_file:
+
+#     data = data_file.readlines()
+
+# print(data)
+
+# for line in data :
+#   new_line =   line.strip()
+#   new_data.append(new_line)
+
+# print(new_data)
+
+# import csv
+
+# with open(r"weather_data.csv") as data_file:
+
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     temp = []
+
+#     for row in data :
+#         x = row[1]
+#         temperatures.append(x)
+
+# del temperatures[0]
+
+# print(temperatures)
+        
+import pandas
