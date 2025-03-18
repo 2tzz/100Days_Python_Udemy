63d3db9b373f935a9e5da40b449de436b36897ae
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-18 18:36:32 +0530

    done 4 today ill se u at tomorrow morning 4 sure

--- Logs/day25/main.py
+++ Logs/day25/main.py
@@ -31,13 +31,29 @@
         
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 
-temp_list = data["temp"].to_list()
+# temp_list = data["temp"].to_list()
 
-# print(data["temp"].mean())
-# print(data["temp"].max())
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
 
-# print(data.temp)
+# # print(data.temp)
 
-print(data[data.temp == data.temp.max()])
+# # print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday" ]
+
+# faren = monday.temp * 9/5 + 32
+
+# print(f"{faren} F")
+
+
+data_dic = {
+    "students" : ["Amy","James","Anjela"] ,
+    "Scores"   :  [76,56,65]
+}
+
+data = pandas.DataFrame(data_dic)
+
+data.to_csv("New_data.csv")
