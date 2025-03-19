e3e7d285188d24bd0f4fbc09c30d46d42ccbe512
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-19 14:46:51 +0530

    starting nato project

--- Logs/day26/list-dict_comprehensions.py
+++ Logs/day26/list-dict_comprehensions.py
@@ -26,12 +26,45 @@ import random
 # print(passed_students)
 
 
-sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 
-word_list = sentence.split()
+# word_list = sentence.split()
 
-print(word_list)
+# print(word_list)
 
-result = {word:len(word)  for word in word_list}
+# result = {word:len(word)  for word in word_list}
 
-print(result)
+# print(result)
+
+# weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+
+# weather_f = { day: int(celcius) * 9/5 +32 for (day, celcius) in weather_c.items()}
+
+# print(weather_f)
+
+
+student_dict = {
+    "student" : ["Angela" , "James" , "Lilly"],
+    "score"   : [56,76,98]
+}
+
+for (key,value) in student_dict.items():
+    print(value)
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+print(student_data_frame)
+
+
+#loop through data frame
+
+# for(key,value) in student_data_frame.items() :
+#     print(value)
+
+
+#loop through rows of a data frame
+
+for (index , row) in student_data_frame.iterrows():
+    print(row.score)
