e73d0da15b535d2a4b803e89d8174000306175f1
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-24 20:01:47 +0530

    diles changed

--- Logs/day30/password_manager_rework/pswd.py
+++ Logs/day30/password_manager_rework/pswd.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import math
 from random import choice , randint , shuffle
 import pyperclip
+import json
 
 
 PINK = "#e2979c"
@@ -10,6 +11,40 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Hobo Std"
+
+# #updating old data with new data
+#                 data.update(new_data)
+
+# ---------------------------- search ------------------------------- #
+
+def search_web():
+
+    website = web_entry_getx.get()
+    email = email_entry_getx.get()
+    password_ = password_entry_getx.get()
+
+    new_data = {
+        website : {
+            "email" : email,
+            "password" : password_,
+        }
+    }
+
+ 
+     
+    with open("passwords.jason" , mode = "r") as file:
+         #reading old data
+        data = json.load(file)
+
+         
+                
+   
+                
+        
+
+   
+
+
 
 
 
@@ -22,7 +57,7 @@ def gen_password():
 
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    symbols = ['!', '#', '$', '%', '&', '*', '+']
 
     password_list = []
 
@@ -47,17 +82,38 @@ def gen_password():
 
 def save_user_data() :
 
-    if len(web_entry_getx.get()) < 2 or len(password_entry_getx.get()) < 2 :
+    website = web_entry_getx.get()
+    email = email_entry_getx.get()
+    password_ = password_entry_getx.get()
+
+    new_data = {
+        website : {
+            "email" : email,
+            "password" : password_,
+        }
+    }
+
+    if len(website) < 2 or len(password_) < 2 :
 
         messagebox.showinfo(title="check the fields" , message="Your webiste data or password is too short !")
 
     else :
-        is_ok = messagebox.askokcancel(title=f"Website : {web_entry_getx.get()}" , message=f"your data will be : \n Email : {email_entry_getx.get()}\n Password:{password_entry_getx.get()}")
+        try:
+            with open("passwords.jason" , mode = "r") as file:
+                #reading old data
+                data = json.load(file)
+                #updating old data with new data
+                data.update(new_data)
+        except:
+            with open("passwords.jason" , mode = "w") as file:  
 
-        if is_ok:
-            with open("passwords.txt" , mode = "a") as file:
-                file.write(f"{web_entry_getx.get()} | {email_entry_getx.get()} | {password_entry_getx.get()}\n")
-                file.close()
+                json.dump(new_data , file  , indent=4)
+            
+        else:
+            with open("passwords.jason" , mode = "w") as file:   
+                #new updated data
+                json.dump(data , file  , indent=4)
+        finally:
             web_entry.delete(0,'end')   
             password_entry.delete(0,'end')
 
@@ -86,9 +142,12 @@ canvas.grid(column=1 , row=0 )
 website = Label(text="Website :",  font=(FONT_NAME,8,"bold"))
 website.grid(column=0 , row=1 ,pady=1)
 
-web_entry = Entry(width=52 , border=1 , background='#e6f3ff' , textvariable=web_entry_getx)
+web_entry = Entry(width=31 , border=1 , background='#e6f3ff' , textvariable=web_entry_getx)
 web_entry.focus()
-web_entry.grid(column=1 , row=1 ,columnspan=2 ,pady=1)
+web_entry.grid(column=1 , row=1 ,columnspan=1 ,pady=1)
+
+search_button = Button(text="search",font=(FONT_NAME,8,"bold"), height=1 ,width=16 , command=gen_password , bg='#d1eaff' ,borderwidth=1 )
+search_button.grid(column=2 , row=1 ,pady=1 , padx=3)
 
 email = Label(text="Email / User_Name :",  font=(FONT_NAME,8,"bold"))
 email.grid(column=0 , row=2 ,pady=1)
