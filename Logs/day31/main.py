398bfd96749a59788541c43e63d5424c1957f59d
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-25 17:05:12 +0530

    created ui

--- Logs/day31/main.py
+++ Logs/day31/main.py
@@ -10,14 +10,26 @@ window = Tk()
 window.title("flashy")
 window.config(padx=20, pady=20 , bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800 , height= 526 , bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas = Canvas(width=800 , height= 550 , bg=BACKGROUND_COLOR, highlightthickness=0  )
+
+
 white_background = PhotoImage(file=r"images\card_front.png")
-canvas.create_image(400, 200 , image=white_background )
-timer_text = canvas.create_text(400 , 150 , text='trouve' , fill="Black" , font=("Ariel",35,"bold"))
-canvas.grid(column=0 , row=0,columnspan=2 ) 
+canvas.create_image(400, 270 , image=white_background )
+
+word_text = canvas.create_text(400 , 270 , text='trouve' , fill="Black" , font=("Ariel",60,"bold"))
+
+title_text = canvas.create_text(400 , 155 , text='Title' , fill="Black" , font=("Ariel",40,"italic"))
+
+canvas.grid(column=0 , row=0, columnspan=2 ,padx=50 , pady=50 ) 
+
+wrong_image = PhotoImage(file=r"images\wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0 , border=0)
+wrong_button.grid(column=0,row=1)
 
 
-
+correct_image = PhotoImage(file=r"images\right.png")
+correct_button = Button(image=correct_image, highlightthickness=0 , border=0)
+correct_button.grid(column=1,row=1)
 
 
 
