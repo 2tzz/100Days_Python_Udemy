4a67fbda51bbc431d17b9d0c67dbdd2d3d05d8a1
2tzz <157218559+2tzz@users.noreply.github.com>
2025-04-07 22:56:26 +0530

    ggs

--- Logs/day37/Habit_Tracker/main.py
+++ Logs/day37/Habit_Tracker/main.py
@@ -1,12 +1,21 @@
 import requests
+from datetime import datetime
 
+today = datetime(year=2025 , month=4 , day=6)
 
 USER_NAME = "thiyura"
 TOKEN = "tT47tt74TT77tt44"
 pixela_endpoint = "https://pixe.la/v1/users"
-graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/graph001"
-pixel_creation_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
+graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs"
+pixel_creation_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/graph001"
+pixel_update_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/graph001/20250406"
+pixel_delete_endpoint = f"{pixela_endpoint}/{USER_NAME}/graphs/graph001/20250406"
 date = "20250407"
+
+
+
+
+
 
 user_params = {
     "token" : TOKEN,
@@ -23,15 +32,19 @@ graph_config = {
 }
 
 pixel_config = {
-    "date" : date,
-    "quantity" : "8"
+    "date" : today.strftime("%Y%m%d"),
+    "quantity" : "15"
 
+}
+
+pixel_config_update  = {
+    "quantity" : "4"
 }
 
 headders = {
     "X-USER-TOKEN" : TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=pixel_config , headers=headders)
-
+# response = requests.put(url=pixel_update_endpoint, json=pixel_config_update , headers=headders)
+response = requests.delete(url=pixel_delete_endpoint , headers=headders)
 print(response.text)
