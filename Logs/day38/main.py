de94518a7501b686b6e613fde6ef7bcfcd6ddb31
2tzz <157218559+2tzz@users.noreply.github.com>
2025-04-09 16:42:53 +0530

    api

---
+++ Logs/day38/main.py
@@ -0,0 +1,43 @@
+import requests
+import datetime
+
+
+natural_exercise_endpoint =  "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+API_KEY = '138e510a7cf838f4b6c90dd45b70538f'
+APP_ID = 'd13ef7a4'
+
+header = {
+    "x-app-id" : APP_ID,
+    "x-app-key" : API_KEY
+}
+
+user_input =  input("Enter the exercises you've done today : ")
+
+exercise_params = {
+    "query": user_input
+}
+
+
+
+response = requests.post(natural_exercise_endpoint , headers=header ,json=exercise_params)
+
+print(response.text)
+
+
+
+
+# url = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+# headers = {
+#     'Content-Type': 'application/json',
+#     'x-app-id': 'd13ef7a4',
+#     'x-app-key': '138e510a7cf838f4b6c90dd45b70538f'
+# }
+# data = {
+#     'query': 'swam for 1 hour'
+# }
+
+# response = requests.post(url, headers=headers, json=data)
+
+# print(response.status_code)
+# print(response.json())
