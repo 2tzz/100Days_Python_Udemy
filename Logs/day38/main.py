439c0c17a346b5ae501b85b512be1dcb2d7babfd
2tzz <157218559+2tzz@users.noreply.github.com>
2025-04-10 13:22:21 +0530

    now its updating to spread sheet

--- Logs/day38/main.py
+++ Logs/day38/main.py
@@ -1,8 +1,16 @@
 import requests
-import datetime
+from datetime import datetime
+
+today = str(datetime.now())
+
+date_list = today.split(" ")
+
+date = date_list[0]
+time = date_list[1].split(".")[0]
 
 
 natural_exercise_endpoint =  "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/4b62b0bf9f98c5e6082ff771ece0e18c/myWorkouts/workouts"
 
 API_KEY = '138e510a7cf838f4b6c90dd45b70538f'
 APP_ID = 'd13ef7a4'
@@ -19,12 +27,34 @@ exercise_params = {
 }
 
 
-
 response = requests.post(natural_exercise_endpoint , headers=header ,json=exercise_params)
 response.raise_for_status()
 calorie_data = response.json()
 
-print(calorie_data)
+calorie_dict = calorie_data["exercises"][0]
+
+
+
+exercise = calorie_dict["name"]
+duration = calorie_dict["duration_min"]
+calories = calorie_dict["nf_calories"]
+
+
+sheet_params = {
+    "Date" : date,
+    "Time" : time,
+    "Exercise" : exercise,
+    "Duration" : duration,
+    "Calories" : calories
+}
+
+header_sheety = {
+    "Authorization": "Bearer TTtt47"
+}
+
+response_sheety = requests.post(sheety_endpoint , headers=header_sheety ,json=sheet_params)
+response_sheety.raise_for_status()
+
 
 
 # url = 'https://trackapi.nutritionix.com/v2/natural/exercise'
