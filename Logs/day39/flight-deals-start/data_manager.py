b2c8d142a1f9050b344ac3885bd8692c65f7d6ae
2tzz <157218559+2tzz@users.noreply.github.com>
2025-04-11 10:04:29 +0530

    api with oop start

--- Logs/day39/flight-deals-start/data_manager.py
+++ Logs/day39/flight-deals-start/data_manager.py
@@ -1,3 +1,22 @@
+import requests
+from pprint import pprint
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.sheety_endpoint = "https://api.sheety.co/4b62b0bf9f98c5e6082ff771ece0e18c/flightDeals/prices"
+        self.header_sheety = {
+            "Authorization": "Bearer flightTt47"
+        }
+    
+
+    def get_data(self) :
+        response_sheety = requests.get(self.sheety_endpoint  , headers=self.header_sheety)
+        response_sheety.raise_for_status()
+        flight_data = response_sheety.json()
+        return flight_data
+
+
+
+
+
+
--- Logs/day39/flight-deals-start/main.py
+++ Logs/day39/flight-deals-start/main.py
@@ -1,1 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from pprint import pprint
+
+dt  = DataManager()
+
+flight_data = dt.get_data()
+pprint(flight_data["prices"])
