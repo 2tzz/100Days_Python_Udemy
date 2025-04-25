917233b6d679378d7ceaee61b009c8553e8ddf5b
2tzz <157218559+2tzz@users.noreply.github.com>
2025-04-25 11:22:38 +0530

    day46 begin

---
+++ Logs/day46/Scraping the Billboard Hot 100/main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+date = '2002-01-05'
+
+URL = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+
+response = requests.get(URL , headers=header)
+soup = BeautifulSoup(response.text, "html.parser")
+
+song_tags = soup.find_all("h3", class_="a-no-trucate")
+
+# Get clean text and remove extra whitespace
+songs = [tag.get_text(strip=True) for tag in song_tags]
+
+# Print only the first 100, in case more are found
+print(songs)
