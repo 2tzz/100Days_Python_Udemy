f9e4aacac44260d5619ced614ad6f3496538b0ba
2tzz <157218559+2tzz@users.noreply.github.com>
2025-05-08 20:03:24 +0530

    dusted

--- Logs/day49/main.py
+++ Logs/day49/main.py
@@ -7,30 +7,30 @@ import time
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
+
 load_dotenv(".env")
 MY_EMAIL = os.getenv("MY_EMAIL")
 MY_PASSWORD = os.getenv("MY_PASSWORD")
 
-# Setup Chrome
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-service = Service("C:\\Development\\chromedriver.exe")  # Adjust path if needed
+service = Service("C:\\Development\\chromedriver.exe")  
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("https://www.linkedin.com/jobs/search?keywords=Python%2BDeveloper&location=Sri%2BLanka&geoId=100446352&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=4223533819&position=24&pageNum=0")
 
-# Wait and click the modal "Sign in" button
+
 wait = WebDriverWait(driver, 10)
 sign_in_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.sign-in-modal__outlet-btn")))
 sign_in_button.click()
 
-# Wait for email input to load
+
 email_field = wait.until(EC.presence_of_element_located((By.ID, "session_key")))
 password_field = wait.until(EC.presence_of_element_located((By.ID, "session_password")))
 login_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="base-sign-in-modal"]/div/section/div/div/form/div[2]/button')))
 
-# Fill credentials and submit
+
 email_field.send_keys(MY_EMAIL)
 password_field.send_keys(MY_PASSWORD)
 login_button.click()
