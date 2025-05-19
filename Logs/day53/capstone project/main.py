556f497cbe342ee7c31e76496bf2645b7d3203b2
2tzz <157218559+2tzz@users.noreply.github.com>
2025-05-19 13:27:27 +0530

    pp

---
+++ Logs/day53/capstone project/main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# Optional - Keep the browser open (helps diagnose issues if the script crashes)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.instagram.com/")
