aafc98f8c09f83ab3e63fd82b43ba9e05de65116
2tzz <157218559+2tzz@users.noreply.github.com>
2025-05-19 15:03:16 +0530

    will be right back

--- Logs/day53/capstone project/main.py
+++ Logs/day53/capstone project/main.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import requests
+from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL_ZILLOW = os.getenv('URL_ZILLOW')
 
 
-# Optional - Keep the browser open (helps diagnose issues if the script crashes)
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome(options=chrome_options)
+# driver.get("https://www.instagram.com/")
 
-driver.get("https://www.instagram.com/")
+response = requests.get(URL_ZILLOW)
+soup = BeautifulSoup(response.text, "html.parser")
