2410483133b8bdfc1839e82b8fa8573dabecf857
2tzz <157218559+2tzz@users.noreply.github.com>
2025-05-20 23:54:52 +0530

    day 53 updated

--- Logs/day53/capstone project/main.py
+++ Logs/day53/capstone project/main.py
@@ -20,17 +20,3 @@ URL_ZILLOW = os.getenv('URL_ZILLOW')
 response = requests.get(URL_ZILLOW)
 soup = BeautifulSoup(response.text, "html.parser")
 
-response = requests.get(URL_ZILLOW)
-soup = BeautifulSoup(response.text, "html.parser")
-
-response = requests.get(URL_ZILLOW)
-soup = BeautifulSoup(response.text, "html.parser")
-
-response = requests.get(URL_ZILLOW)
-soup = BeautifulSoup(response.text, "html.parser")
-
-response = requests.get(URL_ZILLOW)
-soup = BeautifulSoup(response.text, "html.parser")
-
-response = requests.get(URL_ZILLOW)
-soup = BeautifulSoup(response.text, "html.parser")
