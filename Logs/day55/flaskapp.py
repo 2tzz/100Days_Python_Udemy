3909b56bdc32d227c060e6250b474e9bbf9d4689
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-18 18:53:25 +0530

    final project day55

--- Logs/day55/flaskapp.py
+++ Logs/day55/flaskapp.py
@@ -2,9 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1 style= 'text-align : center ' >Hello, World! </h1>" \
+    "<p>this is a paragraph text</p>" \
+    "<img src='https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3Y1Y2gyMDl1NjR3cGZzaW53eGozc2Y4dHF0ZXl1eGR4Z2lvNGh2OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/njtPBlbYnHAHK/giphy.gif' width=200>"
+
 
 @app.route("/<name>")
 def greet_fast(name):
@@ -15,8 +34,9 @@ def greet(name , number):
     return f"Hellow {name} ,you are {number} years old ! "
 
 @app.route("/bye")
+@make_bold
 def say_bye():
-    return "<p>Bye!</p>"
+    return "Bye!"
 
 if __name__ == "__main__" :
     #run in debug mode to auto run server whenever makes a change
---
+++ Logs/day55/test.py
@@ -0,0 +1,38 @@
+# class User :
+#     def __init__(self , name):
+#         self.name = name
+#         self.is_logged_in = False
+
+# def is_authenticated_decorator(function):
+#     def wrapper(*args , **kwargs):
+#         if args[0].is_logged_in == True:
+#           function(args[0]) 
+#         return wrapper
+
+# @is_authenticated_decorator
+# def create_blog_post(user):
+#      print(f"this is {user.name}'s new blog post ")
+
+# new_user = User("angela")
+# new_user.is_logged_in = True
+# create_blog_post(new_user)
+
+
+#exercise
+
+# TODO: Create the logging_decorator() function 👇
+def logging_decorator(func):
+    def wrapper(*args):
+        print(f"You called {func.__name__}{args}")
+        result = func(*args)
+        print(f"It returned: {result}")
+        # return result
+    return wrapper
+
+
+# TODO: Use the decorator 👇
+@logging_decorator
+def a_function(*args):
+    return sum(args)
+    
+a_function(1,2,3)
