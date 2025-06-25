b035ceb7f67109065c0ebf5e910ae9cc5f137fc8
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-26 02:31:35 +0530

    automated year footer

--- Logs/day57/server.py
+++ Logs/day57/server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-
+import random
+from datetime import date
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    random_number = random.randint(0,10)
+    current_date = date.today()
+    current_year = current_date.year
+    return render_template("index.html" , num = random_number , year = current_year)
 
 
 if __name__ == "__main__":
