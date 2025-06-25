7e89b4589cbb8e70f1f6876d81f40d9533a4f879
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-26 03:19:55 +0530

    age api with jinja done

--- Logs/day57/server.py
+++ Logs/day57/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 from datetime import date
+import requests
 
 app = Flask(__name__)
 
@@ -11,6 +12,15 @@ def home_page():
     current_date = date.today()
     current_year = current_date.year
     return render_template("index.html" , num = random_number , year = current_year)
+
+
+@app.route('/guess/<sample_name>')
+def guess_page(sample_name):
+    response = requests.get(url=f"https://api.agify.io/?name={sample_name}" )
+    response.raise_for_status()
+    data = response.json()
+    guessing_age = data['age']
+    return render_template("guess.html" , name = sample_name , age = guessing_age)
 
 
 if __name__ == "__main__":
