cd6b0720f9bec63a2634d026d37e55ba59eb034e
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-26 19:01:32 +0530

    formatted blog.htm with jinja

--- Logs/day57/server.py
+++ Logs/day57/server.py
@@ -28,6 +28,14 @@ def guess_page(sample_name):
 
     return render_template("guess.html" , name = sample_name , age = guessing_age , gender = gender)
 
+@app.route('/blog')
+def blogs_page():
+    blog_url = 'https://api.npoint.io/2ad2d2fb73e1aa796f90'
+    response = requests.get(blog_url)
+    response.raise_for_status()
+    all_posts = response.json()
+    return render_template("blog.html" , posts = all_posts)
+
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
