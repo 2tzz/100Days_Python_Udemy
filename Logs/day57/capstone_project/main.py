510514dfadbd3c7f5ef5f926322db64ebdc56d66
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-26 21:05:28 +0530

    capstone done

--- Logs/day57/capstone_project/main.py
+++ Logs/day57/capstone_project/main.py
@@ -1,16 +1,30 @@
+
 from flask import Flask, render_template
+from post import Post
 import requests
 
+posts = requests.get("https://api.npoint.io/2ad2d2fb73e1aa796f90").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    blog_url = 'https://api.npoint.io/2ad2d2fb73e1aa796f90'
-    response = requests.get(blog_url)
-    response.raise_for_status()
-    all_posts = response.json()   
-    return render_template("index.html" , posts = all_posts)
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
