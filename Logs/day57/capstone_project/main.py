8ebd97dce8d926b298f31ba2e787d3364f179c70
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-26 20:53:27 +0530

    capstone

--- Logs/day57/capstone_project/main.py
+++ Logs/day57/capstone_project/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = 'https://api.npoint.io/2ad2d2fb73e1aa796f90'
+    response = requests.get(blog_url)
+    response.raise_for_status()
+    all_posts = response.json()   
+    return render_template("index.html" , posts = all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
