77183fb070ef897c7b6b73f89866ef79938a8f72
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-28 21:28:57 +0530

    ex

--- Logs/day55/test.py
+++ Logs/day55/test.py
@@ -1,24 +1,24 @@
-# class User :
-#     def __init__(self , name):
-#         self.name = name
-#         self.is_logged_in = False
+class User :
+    def __init__(self , name):
+        self.name = name
+        self.is_logged_in = False
 
-# def is_authenticated_decorator(function):
-#     def wrapper(*args , **kwargs):
-#         if args[0].is_logged_in == True:
-#           function(args[0]) 
-#         return wrapper
+def is_authenticated_decorator(function):
+    def wrapper(*args , **kwargs):
+        if args[0].is_logged_in == True:
+          function(args[0]) 
+        return wrapper
 
-# @is_authenticated_decorator
-# def create_blog_post(user):
-#      print(f"this is {user.name}'s new blog post ")
+@is_authenticated_decorator
+def create_blog_post(user):
+     print(f"this is {user.name}'s new blog post ")
 
-# new_user = User("angela")
-# new_user.is_logged_in = True
-# create_blog_post(new_user)
+new_user = User("angela")
+new_user.is_logged_in = True
+create_blog_post(new_user)
 
 
-#exercise
+# exercise
 
 # TODO: Create the logging_decorator() function 👇
 def logging_decorator(func):
