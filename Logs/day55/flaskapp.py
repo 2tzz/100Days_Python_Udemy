3ae875b1e4d56e7fec27b386aa7b357213f1ee86
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-28 21:27:55 +0530

    day 57

--- Logs/day55/flaskapp.py
+++ Logs/day55/flaskapp.py
@@ -25,19 +25,3 @@ def hello_world():
     "<img src='https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3Y1Y2gyMDl1NjR3cGZzaW53eGozc2Y4dHF0ZXl1eGR4Z2lvNGh2OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/njtPBlbYnHAHK/giphy.gif' width=200>"
 
 
-@app.route("/<name>")
-def greet_fast(name):
-    return f"Hellow there {name} ! "
-
-@app.route("/username/<name>/<int:number>")
-def greet(name , number):
-    return f"Hellow {name} ,you are {number} years old ! "
-
-@app.route("/bye")
-@make_bold
-def say_bye():
-    return "Bye!"
-
-if __name__ == "__main__" :
-    #run in debug mode to auto run server whenever makes a change
-    app.run(debug=True)
