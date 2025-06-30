24a6671a11a3badc7ac9361c877155b6ee3b486a
2tzz <157218559+2tzz@users.noreply.github.com>
2025-06-30 09:14:54 +0530

    question class

--- Logs/day16/ooptest/main.py
+++
@@ -1,9 +0,0 @@
-class Car :
-    def __init__(self):
-        self.color = 'red'
-        self.wheels = 4
-        self.offroad = False
-
-
-    def run(self) :
-        print(f"gellow im {self.color} car")
