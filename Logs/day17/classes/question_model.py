764196193afd73054454006c00c6a42ea678bba8
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-01 11:09:09 +0530

    day58

--- Logs/day17/classes/question_model.py
+++ Logs/day17/classes/question_model.py
@@ -6,6 +6,8 @@ class Question :
         self.answer = correct_answer
 
     
-
+class QuestioonA(Question):
+    def __init__(self, question, correct_answer):
+        super().__init__(question, correct_answer)
     
 
