f90c2e7cc09813f191ffe62415e8a4c65e9fccce
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-07 09:50:57 +0530

    step 5

--- Logs/day59/upgraded_blog/main.py
+++ Logs/day59/upgraded_blog/main.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
+
+@app.route('/about')
+def about_page():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
