5292502ea194289fbf3287074eb8a5f8ec5eb36d
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-07 16:11:19 +0530

    done

--- Logs/day60/forms/main.py
+++ Logs/day60/forms/main.py
@@ -1,16 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
 
-from flask import Flask, render_template, request
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
-
-@app.route("/login", methods=["POST"])
-def receive_data():
-    return "💪 Success! Form submitted"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get data from form
+        username = request.form['username']
+        password = request.form['password']
+        
+        
+    
+    # GET method just renders the form
+    return render_template("success.html", username=username, password=password)
 
 
 if __name__ == "__main__":
