421c159ec32c101ab413b295042e2e097b28ffef
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-07 14:23:59 +0530

    hey

---
+++ Logs/day60/forms/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template("index.html" )
+
+# @app.route('/contact')
+# def contact_page():
+#     return render_template("contact.html")
+
+# @app.route('/about')
+# def about_page():
+#     return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+   
