6ecd66787058fb1808d3cd5c27e8130591e7246f
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-07 10:18:14 +0530

    until rendder separate templates

--- Logs/day59/upgraded_blog/main.py
+++ Logs/day59/upgraded_blog/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import requests
 
 
-response = requests.get('https://api.npoint.io/e6eb6f957cc456f84ab9')
+response = requests.get('https://api.npoint.io/1df9a2e468e2408de0fc')
 response.raise_for_status()
 all_posts = response.json()
 
@@ -22,6 +22,10 @@ def contact_page():
 def about_page():
     return render_template("about.html")
 
+@app.route('/blog/<num>')
+def get_blogs(num):
+
+    return render_template("post.html" , posts = all_posts , number = num)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
