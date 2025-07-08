e134fd84af390b856b3eafa8ebf3b3a196ff5274
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-08 15:38:36 +0530

    wtf forms

--- Logs/day61/main.py
+++ Logs/day61/main.py
@@ -1,16 +1,20 @@
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField , PasswordField
+from wtforms import StringField, PasswordField, SubmitField
+from flask_bootstrap import Bootstrap5 # pip install bootstrap-flask
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField('Password')
+    email = StringField(label='Email')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
-app.secret_key = "haha"
+app.secret_key = "ff"
+bootstrap = Bootstrap5(app) # initialise bootstrap-flask 
+
 
 
 @app.route("/")
