80a8d265a1d1fb9ba98a53eea27ad57153a46b58
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-08 11:52:48 +0530

    wtf form working

--- Logs/day61/main.py
+++ Logs/day61/main.py
@@ -1,12 +1,12 @@
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField , PasswordField
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email')
-    password = StringField('Password')
+    password = PasswordField('Password')
 
 
 app = Flask(__name__)
@@ -18,10 +18,16 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    return render_template('login.html', form=login_form)
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == '__main__':
