92f462ca73d1a9bc2083cdcc3e0c8e2e121b1de3
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-11 22:12:01 +0530

    all things are working perfect

--- Logs/day63/VirtualBookshelf/main.py
+++ Logs/day63/VirtualBookshelf/main.py
@@ -9,7 +9,7 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 
-class User(db.Model):
+class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(unique=True)
     author: Mapped[str]
@@ -20,21 +20,18 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 
-
-
-    
-
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
-    books = db.session.execute(db.select(User)).scalars().all()
+    books = db.session.execute(db.select(Book)).scalars().all()
     return render_template('index.html', books=books)
-
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        new_book = User(
+        new_book = Book(
             title=request.form['title'],
             author=request.form['author'],
             rating=float(request.form['rating'])
@@ -45,16 +42,23 @@ def add():
     return render_template('add.html')
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update():
-    book = db.get_or_404(User, id)
+def update(id):
+    book = db.get_or_404(Book, id)
     if request.method == 'POST':
         book.title = request.form['title']
         book.author = request.form['author']
         book.rating = float(request.form['rating'])
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('index.html', book=book)
+    return render_template('update.html', book=book)
 
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    book = db.get_or_404(Book, id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
