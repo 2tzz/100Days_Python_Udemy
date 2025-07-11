e88a1c42246a0775f00fc7cbb4d2a6ce2f05e426
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-11 14:10:02 +0530

    day 63 434 - ch4

--- Logs/day63/VirtualBookshelf/main.py
+++ Logs/day63/VirtualBookshelf/main.py
@@ -14,13 +14,20 @@ all_books = [
         "title": "Harry Potter",
         "author": "J. K. Rowling",
         "rating": 9,
+    },
+    
+    {
+        "title": "Tale of two cities",
+        "author": "Charles Dickens",
+        "rating": 7,
     }
     ]
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    
+    return render_template('index.html' , books = all_books )
 
 
 @app.route("/add" , methods=['GET', 'POST'] )
