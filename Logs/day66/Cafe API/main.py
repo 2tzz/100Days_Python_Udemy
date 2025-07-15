058681e178426417fcc7ea89509a5f490a24b5e1
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-15 13:08:33 +0530

    serializers

--- Logs/day66/Cafe API/main.py
+++ Logs/day66/Cafe API/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+import random
 
 '''
 Install the required packages first: 
@@ -52,6 +53,51 @@ def home():
 
 
 # HTTP GET - Read Record
+
+@app.route("/random")
+def get_random_cafe():  # Renamed from GET() to follow Python naming conventions
+    all_cafes = Cafe.query.all()
+    random_cafe = random.choice(all_cafes)
+    
+    # Return the random cafe's data as JSON
+    return jsonify(
+        id=random_cafe.id,
+        name=random_cafe.name,
+        map_url=random_cafe.map_url,
+        img_url=random_cafe.img_url,
+        location=random_cafe.location,
+        seats=random_cafe.seats,
+        has_toilet=random_cafe.has_toilet,
+        has_wifi=random_cafe.has_wifi,
+        has_sockets=random_cafe.has_sockets,
+        can_take_calls=random_cafe.can_take_calls,
+        coffee_price=random_cafe.coffee_price
+    )
+
+@app.route("/all")
+def get_all_cafes():  
+    all_cafes = Cafe.query.all()
+    cafes_list =[] 
+    # Return the random cafe's data as JSON
+    for cafe in all_cafes :
+        for cafe in all_cafes:
+            cafe_dict = {
+            "id": cafe.id,
+            "name": cafe.name,
+            "map_url": cafe.map_url,
+            "img_url": cafe.img_url,
+            "location": cafe.location,
+            "seats": cafe.seats,
+            "has_toilet": cafe.has_toilet,
+            "has_wifi": cafe.has_wifi,
+            "has_sockets": cafe.has_sockets,
+            "can_take_calls": cafe.can_take_calls,
+            "coffee_price": cafe.coffee_price
+            }
+        cafes_list.append(cafe_dict)
+        
+    return jsonify(cafes=cafes_list)        
+        
 
 # HTTP POST - Create Record
 
