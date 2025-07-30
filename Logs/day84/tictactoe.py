75ef0c224f6dee51625f2d6571142d442790a2eb
2tzz <157218559+2tzz@users.noreply.github.com>
2025-07-30 09:29:42 +0530

    day 84 begining

---
+++ Logs/day84/tictactoe.py
@@ -0,0 +1,13 @@
+list = [1,2,3,
+        4,5,6,
+        7,8,9]
+
+pattern1 =[ 1,2,3]
+pattern2 =[ 4,5,6]
+pattern3 =[ 7,8,9]
+pattern4 =[ 1,4,7]
+pattern5 =[ 2,5,8]
+pattern6 =[ 3,6,9]
+pattern7 =[ 1,5,9]
+pattern8 =[ 3,5,7]
+
