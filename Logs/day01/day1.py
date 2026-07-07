aab51805b929d826c9bbbcb9b32869d867fd7f34
Thiyura <tthiyura@gmail.com>
2026-07-07 18:55:14 +0530

    begining of something

--- Logs/day01/day1.py
+++ Logs/day01/day1.py
@@ -12,12 +12,18 @@
 # print(len(x) )
 
 # print(len(input("what is your name :")))
-user_name=input("What is your name : ")
-pet_name = input("What is your pets name :")
+# user_name=input("What is your name : ")
+# pet_name = input("What is your pets name :")
 
-print("Your brand name could be "+ user_name + " " +pet_name)
+# print("Your brand name could be "+ user_name + " " +pet_name)
 #ddone
 
+user_name = ""
+user_pet_name = ""
 
+user_name = input("What is your name : ")
+user_pet_name = input("What is your pets name : ")
+
+print("Your new brand name could be " + user_name + " " + user_pet_name)
 
 
